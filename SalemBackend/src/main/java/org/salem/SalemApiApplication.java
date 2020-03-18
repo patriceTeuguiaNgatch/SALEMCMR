bd5f0c5d8e04a87616ce54ca72caec6b2eb1853a
PatriceTeuguiaNgatch <pateuguia@yahoo.com>
2020-03-17 23:49:49 -0400

    initialisation du projet

---
+++ SalemBackend/src/main/java/org/salem/SalemApiApplication.java
@@ -0,0 +1,13 @@
+package org.salem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalemApiApplication.class, args);
+	}
+
+}
---
+++ SalemBackend/src/test/java/org/salem/SalemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.salem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
